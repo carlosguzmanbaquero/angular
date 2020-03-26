7a5d929ce5188e0af6c3e35d91def4a6a237066a
carlosguzmanbaquero <carlosguzmanbaquero@gmail.com>
2020-03-26 16:18:11 -0500

    pipes, router y servicios

---
+++ proyecto011/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { JuegodadosComponent } from './juegodados/juegodados.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: JuegodadosComponent
+  },
+  {
+    path: 'juegodados',
+    component: JuegodadosComponent
+  },
+  {
+    path: 'acercade',
+    component: AcercadeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
