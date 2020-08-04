77d110ae91ae4a8bfb9b3d4d06490ddcdd550172
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-08-03 21:11:58 -0500

    banner con swiper

--- tienda/src/app/app-routing.module.ts
+++ tienda/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { DemoComponent } from './components/demo/demo.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'home',
     component: HomeComponent
   },
