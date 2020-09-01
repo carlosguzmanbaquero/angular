b8c9df6fa26f7b8a0a0511c5a71269f592c8fdee
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-08-31 22:05:46 -0500

    change detection and preload

--- tienda/src/environments/environment.prod.ts
+++ tienda/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store.herokuapp.com',
+  url_api: '',
   firebaseConfig : {
-    apiKey: 'AIzaSyAMs0YZIC69UMtH4EE1W_VCcxTMBK1gKs0',
-    authDomain: 'store-d911a.firebaseapp.com',
-    databaseURL: 'https://store-d911a.firebaseio.com',
-    projectId: 'store-d911a',
-    storageBucket: 'store-d911a.appspot.com',
-    messagingSenderId: '309749293735',
-    appId: '1:309749293735:web:616f312a19616d86aaf182'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
