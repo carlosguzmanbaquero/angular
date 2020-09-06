e86e1056398b5b82ce3ad191ed4e92bf90390b6a
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-09-06 18:45:17 -0500

    service worked firebase message

---
+++ tienda/src/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+});
